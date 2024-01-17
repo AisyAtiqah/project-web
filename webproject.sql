f1ed37a67d0057486f1b267cc147d14ae3331c99
UmairahHamir <156766421+UmairahHamir@users.noreply.github.com>
2024-01-17 13:27:03 +0800

    Add files via upload
    for staff

--- webproject.sql
+++ webproject.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Dec 19, 2023 at 06:20 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.1.12
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 16, 2024 at 06:55 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,41 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `webproject`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `adminId` int(11) NOT NULL,
-  `adminEmail` varchar(200) NOT NULL,
-  `adminUid` varchar(200) NOT NULL,
-  `adminPwd` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`adminId`, `adminEmail`, `adminUid`, `adminPwd`) VALUES
-(1, 'admin123@gmail.com', 'bloodadmin', '$2y$10$5UoXznmqla1vc34CvE.UPe8IyYzQSzGsHx2WRTmXac8nvMRoth7da');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `blood_donors`
---
-
-CREATE TABLE `blood_donors` (
-  `id` int(11) NOT NULL,
-  `usersFK` int(11) NOT NULL,
-  `id_bdFK` int(11) DEFAULT NULL,
-  `Blood` varchar(20) NOT NULL,
-  `Event_Date` date NOT NULL,
-  `Venue` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -74,58 +39,133 @@ CREATE TABLE `blood_event` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `blood_recipient`
+-- Table structure for table `blood_inventory`
 --
 
-CREATE TABLE `blood_recipient` (
-  `br_id` int(11) NOT NULL,
-  `br_bloodtype` varchar(20) NOT NULL,
-  `br_eventdate` date NOT NULL,
-  `br_venue` varchar(255) NOT NULL,
-  `br_medicalhistory` varchar(255) NOT NULL,
-  `usersId` int(11) NOT NULL,
-  `adminId` int(11) NOT NULL
+CREATE TABLE `blood_inventory` (
+  `id` int(30) NOT NULL,
+  `blood_group` varchar(10) NOT NULL,
+  `volume` float NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1 = in -stock,2=out',
+  `donor_id` int(30) NOT NULL,
+  `request_id` int(30) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `blood_inventory`
+--
+
+INSERT INTO `blood_inventory` (`id`, `blood_group`, `volume`, `status`, `donor_id`, `request_id`, `date_created`) VALUES
+(1, 'O+', 450, 1, 3, 0, '2020-10-23 00:00:00'),
+(2, 'AB+', 450, 1, 1, 0, '2020-08-05 00:00:00'),
+(3, 'B-', 450, 1, 2, 0, '2020-10-01 00:00:00'),
+(4, 'AB+', 3, 1, 4, 0, '2024-01-13 00:00:00'),
+(5, 'AB+', 200, 2, 0, 3, '2024-01-13 16:26:01'),
+(6, 'AB-', 3, 1, 5, 0, '2024-01-13 00:00:00'),
+(7, 'AB-', 2, 2, 0, 4, '2024-01-13 16:44:16'),
+(8, 'O+', 1, 1, 6, 0, '2024-01-16 00:00:00'),
+(9, 'O+', 200, 2, 0, 5, '2024-01-16 13:41:40');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `certificate`
+-- Table structure for table `donors`
 --
 
-CREATE TABLE `certificate` (
-  `id` int(11) NOT NULL,
-  `Organizer` varchar(200) NOT NULL,
-  `Password` varchar(200) NOT NULL
+CREATE TABLE `donors` (
+  `id` int(30) NOT NULL,
+  `blood_group` varchar(10) NOT NULL,
+  `name` text NOT NULL,
+  `address` text NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `donors`
+--
+
+INSERT INTO `donors` (`id`, `blood_group`, `name`, `address`, `contact`, `email`, `date_created`) VALUES
+(1, 'AB+', 'John Smith', 'Sample Address', '+18456-5455-55', 'jsmith@sample.com', '2024-10-23 09:25:57'),
+(2, 'B-', 'George Wilson', 'Sample address', '8747808787', 'gwilson@sample.com', '2024-10-23 09:27:54'),
+(3, 'O+', 'Claire Blake', 'Sample Address', '+6948 8542 623', 'cblake@sample.com', '2024-10-23 09:28:14'),
+(4, 'AB+', 'Ezzany Bat', 'ddcsd', '0112304344', 'zany@gmail.com', '2024-01-13 16:24:00'),
+(5, 'AB-', 'Amelia Asyiqin', 'sample', '012343234', 'amelia@gmail.com', '2024-01-13 16:41:23'),
+(6, 'O+', 'Kamarul Asmil', 'No.67, Jalan Kampung Kemunting', '0111233433', 'kamarul@gmail.com', '2024-01-16 13:39:12');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `confirm_attend`
+-- Table structure for table `handedover_request`
 --
 
-CREATE TABLE `confirm_attend` (
-  `id_bd` int(11) NOT NULL,
-  `usersFK` int(11) NOT NULL,
-  `eventFK` int(11) NOT NULL
+CREATE TABLE `handedover_request` (
+  `id` int(30) NOT NULL,
+  `request_id` int(30) NOT NULL,
+  `picked_up_by` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `handedover_request`
+--
+
+INSERT INTO `handedover_request` (`id`, `request_id`, `picked_up_by`, `date_created`) VALUES
+(1, 2, 'Patient Brother', '2024-10-23 13:23:42'),
+(2, 3, 'sister', '2024-01-13 16:26:01'),
+(3, 4, 'umai', '2024-01-13 16:44:16'),
+(4, 5, 'brother', '2024-01-16 13:41:40');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `health_details`
+-- Table structure for table `requests`
 --
 
-CREATE TABLE `health_details` (
-  `id` int(11) NOT NULL,
-  `Blood` varchar(20) NOT NULL,
-  `Weight` float NOT NULL,
-  `Chronic_Health` varchar(200) DEFAULT NULL,
-  `Citizen` varchar(20) NOT NULL,
-  `Others` varchar(20) NOT NULL,
-  `usersFK` int(11) NOT NULL
+CREATE TABLE `requests` (
+  `id` int(30) NOT NULL,
+  `ref_code` varchar(20) NOT NULL,
+  `patient` text NOT NULL,
+  `blood_group` varchar(10) NOT NULL,
+  `volume` float NOT NULL,
+  `physician_name` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 0 COMMENT '0= pending,1= approved',
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `requests`
+--
+
+INSERT INTO `requests` (`id`, `ref_code`, `patient`, `blood_group`, `volume`, `physician_name`, `status`, `date_created`) VALUES
+(2, 'Zfpshiky', 'Mike Williams', 'O+', 450, 'Doctor John', 1, '2024-10-23 10:46:26'),
+(3, 'U0Rmosyw', 'Zulaikha Afza', 'AB+', 200, 'Emy', 1, '2024-01-13 16:25:11'),
+(4, 'G6jT9Rx1', 'Umairah Hamir', 'AB-', 2, 'Patient Sister', 1, '2024-01-13 16:43:12'),
+(5, 'drUsOKWE', 'Asmidah Osman', 'O+', 200, 'test', 1, '2024-01-16 13:41:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `cover_img` text NOT NULL,
+  `about_content` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `cover_img`, `about_content`) VALUES
+(1, 'eDonateLife', 'info@sample.comm', '0112343432', '1603344720_1602738120_pngtree-purple-hd-business-banner-image_5493.jpg', '&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;span style=&quot;color: rgb(0, 0, 0); font-family: &amp;quot;Open Sans&amp;quot;, Arial, sans-serif; font-weight: 400; text-align: justify;&quot;&gt;&amp;nbsp;is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&rsquo;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.&lt;/span&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;br&gt;&lt;/p&gt;&lt;p style=&quot;text-align: center; background: transparent; position: relative;&quot;&gt;&lt;br&gt;&lt;/p&gt;&lt;p&gt;&lt;/p&gt;');
 
 -- --------------------------------------------------------
 
@@ -134,50 +174,24 @@ CREATE TABLE `health_details` (
 --
 
 CREATE TABLE `users` (
-  `usersId` int(11) NOT NULL,
-  `usersEmail` varchar(200) NOT NULL,
-  `usersUid` varchar(200) NOT NULL,
-  `usersPwd` varchar(200) NOT NULL
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 3 COMMENT '1=Admin,2=Staff, 3= subscriber'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `user_details`
+-- Dumping data for table `users`
 --
 
-CREATE TABLE `user_details` (
-  `id` int(11) NOT NULL,
-  `Full_Name` varchar(200) NOT NULL,
-  `Ic` varchar(100) NOT NULL,
-  `Matric` varchar(100) NOT NULL,
-  `Faculty` varchar(50) NOT NULL,
-  `Course` varchar(200) NOT NULL,
-  `Address` varchar(250) NOT NULL,
-  `Telephone` varchar(20) NOT NULL,
-  `Gender` varchar(20) NOT NULL,
-  `Birthday` date NOT NULL,
-  `Age` int(20) NOT NULL,
-  `usersFK` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `type`) VALUES
+(1, 'Administrator', 'admin', '0192023a7bbd73250516f069df18b500', 1),
+(2, 'Umairah Hamir', 'umai', '202cb962ac59075b964b07152d234b70', 2);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`adminId`);
-
---
--- Indexes for table `blood_donors`
---
-ALTER TABLE `blood_donors`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `usersFK` (`usersFK`),
-  ADD KEY `id_bdFK` (`id_bdFK`);
 
 --
 -- Indexes for table `blood_event`
@@ -186,60 +200,44 @@ ALTER TABLE `blood_event`
   ADD PRIMARY KEY (`event_id`);
 
 --
--- Indexes for table `blood_recipient`
+-- Indexes for table `blood_inventory`
 --
-ALTER TABLE `blood_recipient`
-  ADD PRIMARY KEY (`br_id`);
-
---
--- Indexes for table `certificate`
---
-ALTER TABLE `certificate`
+ALTER TABLE `blood_inventory`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `confirm_attend`
+-- Indexes for table `donors`
 --
-ALTER TABLE `confirm_attend`
-  ADD PRIMARY KEY (`id_bd`),
-  ADD KEY `usersFK` (`usersFK`),
-  ADD KEY `eventFK` (`eventFK`);
+ALTER TABLE `donors`
+  ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `health_details`
+-- Indexes for table `handedover_request`
 --
-ALTER TABLE `health_details`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `usersFK` (`usersFK`);
+ALTER TABLE `handedover_request`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `requests`
+--
+ALTER TABLE `requests`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`usersId`);
-
---
--- Indexes for table `user_details`
---
-ALTER TABLE `user_details`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `usersFK` (`usersFK`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `adminId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `blood_donors`
---
-ALTER TABLE `blood_donors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `blood_event`
@@ -248,52 +246,40 @@ ALTER TABLE `blood_event`
   MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `blood_recipient`
+-- AUTO_INCREMENT for table `blood_inventory`
 --
-ALTER TABLE `blood_recipient`
-  MODIFY `br_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `blood_inventory`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `certificate`
+-- AUTO_INCREMENT for table `donors`
 --
-ALTER TABLE `certificate`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `donors`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `confirm_attend`
+-- AUTO_INCREMENT for table `handedover_request`
 --
-ALTER TABLE `confirm_attend`
-  MODIFY `id_bd` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `handedover_request`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- Constraints for dumped tables
+-- AUTO_INCREMENT for table `requests`
 --
+ALTER TABLE `requests`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- Constraints for table `blood_donors`
+-- AUTO_INCREMENT for table `system_settings`
 --
-ALTER TABLE `blood_donors`
-  ADD CONSTRAINT `blood_donors_ibfk_1` FOREIGN KEY (`usersFK`) REFERENCES `users` (`usersId`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `blood_donors_ibfk_2` FOREIGN KEY (`id_bdFK`) REFERENCES `confirm_attend` (`id_bd`) ON DELETE SET NULL ON UPDATE SET NULL;
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Constraints for table `confirm_attend`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `confirm_attend`
-  ADD CONSTRAINT `confirm_attend_ibfk_1` FOREIGN KEY (`usersFK`) REFERENCES `user_details` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `confirm_attend_ibfk_2` FOREIGN KEY (`eventFK`) REFERENCES `blood_event` (`event_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `health_details`
---
-ALTER TABLE `health_details`
-  ADD CONSTRAINT `health_details_ibfk_1` FOREIGN KEY (`usersFK`) REFERENCES `users` (`usersId`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `user_details`
---
-ALTER TABLE `user_details`
-  ADD CONSTRAINT `user_details_ibfk_1` FOREIGN KEY (`usersFK`) REFERENCES `users` (`usersId`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
