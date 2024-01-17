cc976ff7d875f4ab8150c3cfb43ba61e67a49b9f
UmairahHamir <156766421+UmairahHamir@users.noreply.github.com>
2024-01-17 13:32:14 +0800

    Update db_connect.php

--- eDonateLifeStaff/db_connect.php
+++ eDonateLifeStaff/db_connect.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "webproject";
+$dbname = "webproject_2";
 
 //create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
