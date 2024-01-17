9c430f54cb4549ceb17faf8885f7fc32213e1af8
UmairahHamir <156766421+UmairahHamir@users.noreply.github.com>
2024-01-17 13:25:42 +0800

    Add files via upload

---
+++ eDonateLifeStaff/header.php
@@ -0,0 +1,60 @@
+<meta content="" name="descriptison">
+  <meta content="" name="keywords">
+  
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link rel="stylesheet" href="layout/font-awesome/css/all.min.css">
+
+  <!-- Blood CSS Files -->
+  <link href="layout/blood/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <link href="layout/blood/icofont/icofont.min.css" rel="stylesheet">
+
+  <link href="layout/blood/boxicons/css/boxicons.min.css" rel="stylesheet">
+
+  <link href="layout/blood/venobox/venobox.css" rel="stylesheet">
+
+  <link href="layout/blood/animate.css/animate.min.css" rel="stylesheet">
+
+  <link href="layout/blood/remixicon/remixicon.css" rel="stylesheet">
+
+  <link href="layout/blood/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  
+  <link href="layout/blood/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
+  
+  <link href="layout/DataTables/datatables.min.css" rel="stylesheet">
+  
+  <link href="layout/css/jquery.datetimepicker.min.css" rel="stylesheet">
+  <link href="layout/css/select2.min.css" rel="stylesheet">
+
+
+  <!-- Template Main CSS File -->
+  <link href="layout/css/style.css" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="assets/css/jquery-te-1.4.0.css">
+  
+  <script src="layout/blood/jquery/jquery.min.js"></script>
+
+  <script src="layout/DataTables/datatables.min.js"></script>
+
+  <script src="layout/blood/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <script src="layout/blood/jquery.easing/jquery.easing.min.js"></script>
+
+  <script src="layout/blood/php-email-form/validate.js"></script>
+
+  <script src="layout/blood/venobox/venobox.min.js"></script>
+
+  <script src="layout/blood/waypoints/jquery.waypoints.min.js"></script>
+
+  <script src="layout/blood/counterup/counterup.min.js"></script>
+
+  <script src="layout/blood/owl.carousel/owl.carousel.min.js"></script>
+
+  <script src="layout/blood/bootstrap-datepicker/js/bootstrap-datepicker.min.js"></script>
+    <script type="text/javascript" src="layout/js/select2.min.js"></script>
+    <script type="text/javascript" src="layout/js/jquery.datetimepicker.full.min.js"></script>
+    <script type="text/javascript" src="layout/font-awesome/js/all.min.js"></script>
+  <script type="layout/javascript" src="layout/js/jquery-te-1.4.0.min.js" charset="utf-8"></script>
+
+
+
